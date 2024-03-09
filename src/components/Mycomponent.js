33ac4e8464804074a75893b9b55a41bafc0af976
Phuoc Nguyen <nguyen.huuphuoc147@gmail.com>
2024-03-09 22:34:17 +0700

    components

---
+++ src/components/Mycomponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+    render() {
+        return (
+            <div>This is Component</div>
+        );
+
+    }
+}
+
+export default MyComponent;
